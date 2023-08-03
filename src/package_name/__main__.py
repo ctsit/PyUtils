5ba68ce555db597631e40a76276ab4d101d515ad
Michael Bentz <75623654+mbentz-uf@users.noreply.github.com>
2023-08-03 08:56:15 -0400

    Initial commit

---
+++ src/package_name/__main__.py
@@ -0,0 +1,10 @@
+def main():
+    print("process arguments here")
+
+
+def hello_world():
+    return ('hello world')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/package_name/test__main__.py
@@ -0,0 +1,15 @@
+import unittest
+
+from package_name import __main__
+
+
+class TestMain(unittest.TestCase):
+    def test_return_hello_world(self):
+        expected = 'hello world'
+        actual = __main__.hello_world()
+
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
