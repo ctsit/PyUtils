b997e25784a8c4a03ff810dc6c1ee7aa3b3d16f6
Michael Bentz <mbentz@ufl.edu>
2023-08-03 10:19:05 -0400

    Add docstrings to functions

--- src/py_utils/utils.py
+++ src/py_utils/utils.py
@@ -5,10 +5,18 @@ from email.message import EmailMessage
 from email.policy import SMTP
 
 
-def send_email(sender, recipients, subject, body=None, file=None, output=None):
-    """
-    This script sends an email using smtp.ufl.edu. It takes arguments like Sender(required),
-    Recipient(required), Attachment, Subject(required), Message.
+def send_email(
+        sender: str, recipients: list[str], subject: str, body: str | None = None,
+        file: str | None = None, output=None):
+    """Sends an email using smtp.ufl.edu.
+
+    Args:
+        sender (str): The sender of the email, i.e., the "From" field.
+        recipients (list[str]): The recipients to send the emails to.
+        subject (str): The subject line of the email.
+        body (str | None): The body of the email.
+        file (str | None): The path to the file to send as an attachment
+        output:
     """
     msg = EmailMessage()
     msg["Subject"] = subject
@@ -30,11 +38,27 @@ def send_email(sender, recipients, subject, body=None, file=None, output=None):
             smtp.send_message(msg)
 
 
-def prevent_overwrite(filename):
-    if not os.path.exists(filename):
-        return filename
+def get_unique_filename(path_to_file: str) -> str:
+    """Get a unique filename.
 
-    name, extension = os.path.splitext(filename)
+    Checks if the file exists, and if the file exists the function returns a unique
+    incremented filename. If the file doesn't exist the function returns the `path_to_file`
+    that was provided. For example, if `my_file.pdf` already exists the function will return
+    `my_file (1).pdf`, otherwise `my_file.pdf` is returned.
+
+    Args:
+        path_to_file (str): The path to the file to check.
+
+    Returns:
+        A unique filename.
+
+    Raises:
+        OSError: If there is an error accessing the directory.
+    """
+    if not os.path.exists(path_to_file):
+        return path_to_file
+
+    name, extension = os.path.splitext(path_to_file)
 
     count = 1
 
--- tests/py_utils/test_utils.py
+++ tests/py_utils/test_utils.py
@@ -5,24 +5,25 @@ from py_utils import utils
 
 
 class TestUtils(unittest.TestCase):
-    original_file_name = 'prevent_overwrite_test.file'
-    first_duplicate_file_name = 'prevent_overwrite_test (1).file'
-    second_duplicate_file_name = 'prevent_overwrite_test (2).file'
+    original_file_name = 'get_unique_filename_test.file'
+    first_duplicate_file_name = 'get_unique_filename_test (1).file'
+    second_duplicate_file_name = 'get_unique_filename_test (2).file'
 
-    def test_prevent_overwrite_base(self):
-        self.assertEquals(utils.prevent_overwrite(self.original_file_name), self.original_file_name)
+    def test_get_unique_filename_base(self):
+        self.assertEquals(utils.get_unique_filename(
+            self.original_file_name), self.original_file_name)
 
-    def test_prevent_overwrite_first(self):
+    def test_get_unique_filename_first(self):
         open(self.original_file_name, 'w').close()
         self.assertEquals(
-            utils.prevent_overwrite(self.original_file_name),
+            utils.get_unique_filename(self.original_file_name),
             self.first_duplicate_file_name)
 
-    def test_prevent_overwrite_second(self):
+    def test_get_unique_filename_second(self):
         open(self.original_file_name, 'w').close()
         open(self.first_duplicate_file_name, 'w').close()
         self.assertEquals(
-            utils.prevent_overwrite(self.original_file_name),
+            utils.get_unique_filename(self.original_file_name),
             self.second_duplicate_file_name)
 
     def tearDown(self):
